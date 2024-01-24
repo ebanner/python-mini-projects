bd8fc618f0d450b7ea5d1918a0b616c4288b664e
Edward Banner <edward.banner@gmail.com>
2024-01-24 17:46:15 -0500

    Add venv script

---
+++ python-venv/venv.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+echo 'SYSTEM PYTHON3'
+echo
+
+
+python3 -m venv .venv
+
+python3_path=$(which python3)
+pip3_path=$(which pip3)
+
+echo 'python3 →' $python3_path
+echo 'pip3 →' $pip3_path
+
+source .venv/bin/activate
+
+echo
+echo 'VENV ACTIVATED'
+echo
+
+python3_path=$(which python3)
+pip3_path=$(which pip3)
+
+echo 'python3 →' $python3_path
+echo 'pip3 →' $pip3_path
+
+deactivate
+
+echo
+echo 'VENV DEACTIVATED'
+echo
+
+python3_path=$(which python3)
+pip3_path=$(which pip3)
+
+echo 'python3 →' $python3_path
+echo 'pip3 →' $pip3_path
