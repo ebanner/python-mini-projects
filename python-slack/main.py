b82d3612ef1c16bf144e2b4c79d692e2614534b1
Edward Banner <edward.banner@gmail.com>
2025-11-14 09:24:46 -0500

    Update main.py

--- python-slack/main.py
+++ python-slack/main.py
@@ -1,19 +1,12 @@
-import os
-
-from dotenv import load_dotenv
-load_dotenv()
-
 from slack_sdk import WebClient
 
-SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+SLACK_BOT_TOKEN = ...
 
 slack_client = WebClient(SLACK_BOT_TOKEN)
 
+response = slack_client.chat_postMessage(
+    channel='general',
+    text='Hello!'
+)
 
-if __name__ == '__main__':
-    response = slack_client.chat_postMessage(
-        channel='general',
-        text='Hello!'
-    )
-
-    print(response.data)
+response.data
